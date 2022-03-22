6ee880920399de9a74fd3a75fa794fb6ac3336f6
THKIM <rlaxogus505@naver.com>
2022-03-22 13:45:51 +0000

    22.03.22

---
+++ Programmers/.ipynb_checkpoints/숫자 문자열과 영단어-checkpoint.py
@@ -0,0 +1,12 @@
+def solution(s):
+
+    num = ["zero", "one", "two", "three",
+          "four", "five", "six", "seven",
+          "eight", "nine"]
+
+    for i in range(len(num)):
+        if num[i] in s:
+            s = s.replace(num[i], str(i))
+
+    answer = int(s)
+    return answer
