361e54cd55a49a727b02181a661df766f7ba53b5
THKIM <rlaxogus505@naver.com>
2022-05-12 08:18:23 +0000

    22.05.12

--- Baekjoon/Tree/14675.py
+++ Baekjoon/Tree/14675.py
@@ -1,3 +1,21 @@
 import sys
 input = lambda : sys.stdin.readline().rstrip()
+from collections import defaultdict
 
+N = int(input())
+dict_ = defaultdict(list)
+
+for _ in range(N-1):
+    i, k = map(int, input().split())
+    dict_[i].append(k)
+    dict_[k].append(i)
+    
+q = int(input())
+for _ in range(q):
+    t, k = map(int, input().split())
+    dict_2 = dict_.copy()
+    if t == 1:
+        if len(dict_[k]) == 1:
+            print("no")
+            continue
+    print("yes")
